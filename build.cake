187d65a10574aaeddfff7870815991ec04e93061
salda8 <saldicek@gmail.com>
2018-07-31 12:15:23 +0200

    Running tests. Paket fixes.

--- build.cake
+++ build.cake
@@ -1,186 +1,98 @@
-// The MIT License (MIT)
-// 
-// Copyright (c) 2015-2017 Rasmus Mikkelsen
-// Copyright (c) 2015-2017 eBay Software Foundation
-// https://github.com/eventflow/EventFlow.MongoDb
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy of
-// this software and associated documentation files (the "Software"), to deal in
-// the Software without restriction, including without limitation the rights to
-// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// the Software, and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// 
+#tool paket:?package=NUnit.ConsoleRunner&group=main
+#addin paket:?package=Cake.Figlet&group=build/setup
 
-#r "System.IO.Compression.FileSystem"
-#r "System.Xml"
+// You don't need to specify a group in the URI because you put it in the tools group
+#tool paket:?package=JetBrains.ReSharper.CommandLineTools
 
-#tool "nuget:?package=NUnit.ConsoleRunner"
-#tool "nuget:?package=OpenCover"
+// This is only needed if you want to use paket commands like PaketPack(...) and/or PaketPush(...)
+// You don't need to specify a group in the URI because you put it in the addins group
+#addin paket:?package=Cake.Paket
 
-using System.IO.Compression;
-using System.Net;
-using System.Xml;
+//////////////////////////////////////////////////////////////////////
+// ARGUMENTS
+//////////////////////////////////////////////////////////////////////
 
-var VERSION = GetArgumentVersion();
-var PROJECT_DIR = Context.Environment.WorkingDirectory.FullPath;
-var CONFIGURATION = "Release";
-var REGEX_NUGETPARSER = new System.Text.RegularExpressions.Regex(
-    @"(?<group>[a-z]+)\s+(?<package>[a-z\.0-9]+)\s+\-\s+(?<version>[0-9\.]+)",
-    System.Text.RegularExpressions.RegexOptions.IgnoreCase | System.Text.RegularExpressions.RegexOptions.Compiled);
+var target = Argument("target", "Default");
+var configuration = Argument("configuration", "Release");
 
-// IMPORTANT DIRECTORIES
-var DIR_OUTPUT_PACKAGES = System.IO.Path.Combine(PROJECT_DIR, "Build", "Packages");
-var DIR_OUTPUT_REPORTS = System.IO.Path.Combine(PROJECT_DIR, "Build", "Reports");
+//////////////////////////////////////////////////////////////////////
+// PREPARATION
+//////////////////////////////////////////////////////////////////////
 
-// IMPORTANT FILES
-var FILE_SOLUTION = System.IO.Path.Combine(PROJECT_DIR, "EventFlow.MongoDB.sln");
+// Define directories.
+var buildDir = Directory("./src/Example/bin") + Directory(configuration);
+var reports = "./Reports";
+var nuGet = "./NuGet";
 
-var RELEASE_NOTES = ParseReleaseNotes(System.IO.Path.Combine(PROJECT_DIR, "RELEASE_NOTES.md"));
+//////////////////////////////////////////////////////////////////////
+// TASKS
+//////////////////////////////////////////////////////////////////////
 
-// =====================================================================================================
-Task("Default")
-    .IsDependentOn("Package");
+Setup(context => 
+{
+    Information(Figlet("Cake.Paket.Example"));
+});
 
-// =====================================================================================================
 Task("Clean")
     .Does(() =>
-        {
-            CleanDirectories(new []
-                {
-                    DIR_OUTPUT_PACKAGES,
-                    DIR_OUTPUT_REPORTS
-                });
-				
-			DeleteDirectories(GetDirectories("**/bin"), true);
-			DeleteDirectories(GetDirectories("**/obj"), true);
-        });
-	
-// =====================================================================================================
-Task("Restore")
+{
+    CleanDirectories(new[] {buildDir, reports, nuGet});
+});
+
+Task("Build")
     .IsDependentOn("Clean")
     .Does(() =>
-        {
-			DotNetCoreRestore(
-				".", 
-				new DotNetCoreRestoreSettings()
-				{
-					ArgumentCustomization = aggs => aggs.Append(GetDotNetCoreArgsVersions())
-				});
-        });
-		
-// =====================================================================================================
-Task("Build")
-    .IsDependentOn("Restore")
-    .Does(() =>
-        {
-            DotNetCoreBuild(
-				".", 
-				new DotNetCoreBuildSettings()
-				{
-					Configuration = CONFIGURATION,
-					ArgumentCustomization = aggs => aggs
-                        .Append(GetDotNetCoreArgsVersions())
-                        .Append("/p:ci=true")
-                        .Append("/p:SourceLinkEnabled=true")
-				});
-        });
-
-// =====================================================================================================
-Task("Package")
-    .IsDependentOn("Build")
-    .Does(() =>
-        {
-            Information("Version: {0}", RELEASE_NOTES.Version);
-            Information(string.Join(Environment.NewLine, RELEASE_NOTES.Notes));
-
-			foreach (var project in GetFiles("./Source/**/*.csproj"))
-			{
-				var name = project.GetDirectory().FullPath;
-				var version = VERSION.ToString();
-				
-				if ((name.Contains("Test") && !name.Contains("TestHelpers")) || name.Contains("Example"))
-				{
-					continue;
-				}
-
-                SetReleaseNotes(project.ToString());
-							
-				DotNetCorePack(
-					name,
-					new DotNetCorePackSettings()
-					{
-						Configuration = CONFIGURATION,
-						OutputDirectory = DIR_OUTPUT_PACKAGES,
-						NoBuild = true,
-						ArgumentCustomization = aggs => aggs.Append(GetDotNetCoreArgsVersions())
-					});
-			}
-        });
-
-// =====================================================================================================
-Task("All")
-    .IsDependentOn("Package")
-    .Does(() =>
-        {
-
-        });
-
-// =====================================================================================================
-
-Version GetArgumentVersion()
 {
-    return Version.Parse(EnvironmentVariable("APPVEYOR_BUILD_VERSION") ?? "0.0.1");
-}
-
-string GetDotNetCoreArgsVersions()
-{
-	var version = GetArgumentVersion().ToString();
-	
-	return string.Format(
-		@"/p:Version={0} /p:AssemblyVersion={0} /p:FileVersion={0} /p:ProductVersion={0}",
-		version);
-}
-
-void SetReleaseNotes(string filePath)
-{
-    var releaseNotes = string.Join(Environment.NewLine, RELEASE_NOTES.Notes);
-
-    var xmlDocument = new XmlDocument();
-    xmlDocument.Load(filePath);
-
-    var node = xmlDocument.SelectSingleNode("Project/PropertyGroup/PackageReleaseNotes") as XmlElement;
-    if (node == null)
+    if(IsRunningOnWindows())
     {
-        throw new Exception(string.Format(
-            "Project {0} does not have a `<PackageReleaseNotes>UPDATED BY BUILD</PackageReleaseNotes>` property",
-            filePath));
+      // Use MSBuild
+      MSBuild("./src/Example.sln", settings =>
+        settings.SetConfiguration(configuration));
     }
-
-    if (!AppVeyor.IsRunningOnAppVeyor)
-    {
-        Information("Skipping update of release notes");
-        return;
-    } 
     else
     {
-        Information(string.Format("Setting release notes in '{0}'", filePath));
-        
-        node.InnerText = releaseNotes;
-
-        xmlDocument.Save(filePath);
+      // Use XBuild
+      XBuild("./src/Example.sln", settings =>
+        settings.SetConfiguration(configuration));
     }
-}
+});
 
+Task("Run-Unit-Tests")
+    .IsDependentOn("Build")
+    .Does(() =>
+{
+    NUnit3("./src/**/bin/" + configuration + "/*.Tests.dll", new NUnit3Settings {
+        NoResults = true
+        });
+});
 
-RunTarget(Argument<string>("target", "Package"));
+Task("Run-DupFinder")
+    .IsDependentOn("Build")
+    .Does(() =>
+{
+    if(IsRunningOnWindows())
+    {
+        EnsureDirectoryExists(reports);
+        DupFinder("./src/Example.sln", new DupFinderSettings { ShowStats = true, ShowText = true, OutputFile = reports + "/dupFinder.xml" });
+    }
+});
+
+// Uses the Cake.Paket addin to create a NuGet package
+Task("Paket-Pack").IsDependentOn("Build").Does(() =>
+{
+    EnsureDirectoryExists(nuGet);
+    PaketPack(nuGet, new PaketPackSettings { Version = "1.0.0" });
+});
+
+//////////////////////////////////////////////////////////////////////
+// TASK TARGETS
+//////////////////////////////////////////////////////////////////////
+
+Task("Default")
+    .IsDependentOn("Run-Unit-Tests").IsDependentOn("Run-DupFinder").IsDependentOn("Paket-Pack");
+
+//////////////////////////////////////////////////////////////////////
+// EXECUTION
+//////////////////////////////////////////////////////////////////////
+
+RunTarget(target);
--- build.ps1
+++ build.ps1
@@ -1,18 +1,17 @@
 ##########################################################################
 # This is the Cake bootstrapper script for PowerShell.
-# This file was downloaded from https://github.com/cake-build/resources
+# This version was download from https://github.com/larzw/Cake.Paket
+# It was modified to use paket (instead of NuGet) for dependency management.
 # Feel free to change this file to fit your needs.
 ##########################################################################
 
 <#
-
 .SYNOPSIS
 This is a Powershell script to bootstrap a Cake build.
-
 .DESCRIPTION
-This Powershell script will download NuGet if missing, restore NuGet tools (including Cake)
+This Powershell script will download paket.exe if missing,
+install all your dependencies via paket.exe restore
 and execute your Cake build script with the parameters you provide.
-
 .PARAMETER Script
 The build script to execute.
 .PARAMETER Target
@@ -28,14 +27,20 @@ Performs a dry run of the build script.
 No tasks will be executed.
 .PARAMETER Mono
 Tells Cake to use the Mono scripting engine.
-.PARAMETER SkipToolPackageRestore
-Skips restoring of packages.
+.PARAMETER Paket
+The relative path to the .paket directory.
+.PARAMETER Cake
+The relative path to directory containing Cake.exe.
+.PARAMETER Tools
+The relative path to the Cake tools directory.
+.PARAMETER Addins
+The relative path to the Cake addins directory.
+.PARAMETER Modules
+The relative path to the Cake modules directory.
 .PARAMETER ScriptArgs
 Remaining arguments are added here.
-
 .LINK
-https://cakebuild.net
-
+http://cakebuild.net
 #>
 
 [CmdletBinding()]
@@ -50,52 +55,17 @@ Param(
     [Alias("DryRun","Noop")]
     [switch]$WhatIf,
     [switch]$Mono,
-    [switch]$SkipToolPackageRestore,
+    [ValidatePattern('.paket$')]
+    [string]$Paket = ".\.paket",
+    [string]$Cake = ".\packages\tools\Cake",
+    [string]$Tools = ".\packages\tools",
+    [string]$Addins = ".\packages\addins",
+    [string]$Modules = ".\packages\modules",
     [Parameter(Position=0,Mandatory=$false,ValueFromRemainingArguments=$true)]
     [string[]]$ScriptArgs
 )
 
-[Reflection.Assembly]::LoadWithPartialName("System.Security") | Out-Null
-function MD5HashFile([string] $filePath)
-{
-    if ([string]::IsNullOrEmpty($filePath) -or !(Test-Path $filePath -PathType Leaf))
-    {
-        return $null
-    }
-
-    [System.IO.Stream] $file = $null;
-    [System.Security.Cryptography.MD5] $md5 = $null;
-    try
-    {
-        $md5 = [System.Security.Cryptography.MD5]::Create()
-        $file = [System.IO.File]::OpenRead($filePath)
-        return [System.BitConverter]::ToString($md5.ComputeHash($file))
-    }
-    finally
-    {
-        if ($file -ne $null)
-        {
-            $file.Dispose()
-        }
-    }
-}
-
 Write-Host "Preparing to run build script..."
-
-if(!$PSScriptRoot){
-    $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
-}
-
-$TOOLS_DIR = Join-Path $PSScriptRoot "tools"
-$ADDINS_DIR = Join-Path $TOOLS_DIR "Addins"
-$MODULES_DIR = Join-Path $TOOLS_DIR "Modules"
-$NUGET_EXE = Join-Path $TOOLS_DIR "nuget.exe"
-$CAKE_EXE = Join-Path $TOOLS_DIR "Cake/Cake.exe"
-$NUGET_URL = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-$PACKAGES_CONFIG = Join-Path $TOOLS_DIR "packages.config"
-$PACKAGES_CONFIG_MD5 = Join-Path $TOOLS_DIR "packages.config.md5sum"
-$ADDINS_PACKAGES_CONFIG = Join-Path $ADDINS_DIR "packages.config"
-$MODULES_PACKAGES_CONFIG = Join-Path $MODULES_DIR "packages.config"
 
 # Should we use mono?
 $UseMono = "";
@@ -117,110 +87,76 @@ if($WhatIf.IsPresent) {
     $UseDryRun = "-dryrun"
 }
 
-# Make sure tools folder exists
-if ((Test-Path $PSScriptRoot) -and !(Test-Path $TOOLS_DIR)) {
-    Write-Verbose -Message "Creating tools directory..."
-    New-Item -Path $TOOLS_DIR -Type directory | out-null
-}
+Write-Verbose -Message "Using paket for dependency management..."
 
-# Make sure that packages.config exist.
-if (!(Test-Path $PACKAGES_CONFIG)) {
-    Write-Verbose -Message "Downloading packages.config..."
-    try { (New-Object System.Net.WebClient).DownloadFile("https://cakebuild.net/download/bootstrapper/packages", $PACKAGES_CONFIG) } catch {
-        Throw "Could not download packages.config."
+# Make sure the .paket directory exits
+$PaketDir = Resolve-Path $Paket
+if(!(Test-Path $PaketDir)) {
+    Throw "Could not find .paket directory at $PaketDir"
+}
+Write-Verbose -Message "Found .paket in PATH at $PaketDir"
+
+# Set paket directory enviornment variable
+$ENV:PAKET = $PaketDir
+
+# If paket.exe does not exits then download it using paket.bootstrapper.exe
+$PAKET_EXE = Join-Path $PaketDir "paket.exe"
+if (!(Test-Path $PAKET_EXE)) {
+    # If paket.bootstrapper.exe exits then run it.
+    $PAKET_BOOTSTRAPPER_EXE = Join-Path $PaketDir "paket.bootstrapper.exe"
+    if (!(Test-Path $PAKET_BOOTSTRAPPER_EXE)) {
+        Throw "Could not find paket.bootstrapper.exe at $PAKET_BOOTSTRAPPER_EXE"
     }
-}
+    Write-Verbose -Message "Found paket.bootstrapper.exe in PATH at $PAKET_BOOTSTRAPPER_EXE"
 
-# Try find NuGet.exe in path if not exists
-if (!(Test-Path $NUGET_EXE)) {
-    Write-Verbose -Message "Trying to find nuget.exe in PATH..."
-    $existingPaths = $Env:Path -Split ';' | Where-Object { (![string]::IsNullOrEmpty($_)) -and (Test-Path $_ -PathType Container) }
-    $NUGET_EXE_IN_PATH = Get-ChildItem -Path $existingPaths -Filter "nuget.exe" | Select -First 1
-    if ($NUGET_EXE_IN_PATH -ne $null -and (Test-Path $NUGET_EXE_IN_PATH.FullName)) {
-        Write-Verbose -Message "Found in PATH at $($NUGET_EXE_IN_PATH.FullName)."
-        $NUGET_EXE = $NUGET_EXE_IN_PATH.FullName
-    }
-}
+    # Download paket.exe
+    Write-Verbose -Message "Running paket.bootstrapper.exe to download paket.exe"
+    Invoke-Expression $PAKET_BOOTSTRAPPER_EXE
 
-# Try download NuGet.exe if not exists
-if (!(Test-Path $NUGET_EXE)) {
-    Write-Verbose -Message "Downloading NuGet.exe..."
-    try {
-        (New-Object System.Net.WebClient).DownloadFile($NUGET_URL, $NUGET_EXE)
-    } catch {
-        Throw "Could not download NuGet.exe."
+    if (!(Test-Path $PAKET_EXE)) {
+        Throw "Could not find paket.exe at $PAKET_EXE"
     }
 }
+Write-Verbose -Message "Found paket.exe in PATH at $PAKET_EXE"
 
-# Save nuget.exe path to environment to be available to child processed
-$ENV:NUGET_EXE = $NUGET_EXE
+# Install the dependencies
+Write-Verbose -Message "Running paket.exe restore"
+Invoke-Expression "$PAKET_EXE restore"
 
-# Restore tools from NuGet?
-if(-Not $SkipToolPackageRestore.IsPresent) {
-    Push-Location
-    Set-Location $TOOLS_DIR
-
-    # Check for changes in packages.config and remove installed tools if true.
-    [string] $md5Hash = MD5HashFile($PACKAGES_CONFIG)
-    if((!(Test-Path $PACKAGES_CONFIG_MD5)) -Or
-      ($md5Hash -ne (Get-Content $PACKAGES_CONFIG_MD5 ))) {
-        Write-Verbose -Message "Missing or changed package.config hash..."
-        Remove-Item * -Recurse -Exclude packages.config,nuget.exe
-    }
-
-    Write-Verbose -Message "Restoring tools from NuGet..."
-    $NuGetOutput = Invoke-Expression "&`"$NUGET_EXE`" install -ExcludeVersion -OutputDirectory `"$TOOLS_DIR`""
-
-    if ($LASTEXITCODE -ne 0) {
-        Throw "An error occured while restoring NuGet tools."
-    }
-    else
-    {
-        $md5Hash | Out-File $PACKAGES_CONFIG_MD5 -Encoding "ASCII"
-    }
-    Write-Verbose -Message ($NuGetOutput | out-string)
-    
-    Pop-Location
+# tools
+if (Test-Path $Tools) {
+    $ToolsDir = Resolve-Path $Tools
+    $ENV:CAKE_PATHS_TOOLS =  $ToolsDir
+}
+else {
+    Write-Verbose -Message "Could not find tools directory at $Tools"
 }
 
-# Restore addins from NuGet
-if (Test-Path $ADDINS_PACKAGES_CONFIG) {
-    Push-Location
-    Set-Location $ADDINS_DIR
-
-    Write-Verbose -Message "Restoring addins from NuGet..."
-    $NuGetOutput = Invoke-Expression "&`"$NUGET_EXE`" install -ExcludeVersion -OutputDirectory `"$ADDINS_DIR`""
-
-    if ($LASTEXITCODE -ne 0) {
-        Throw "An error occured while restoring NuGet addins."
-    }
-
-    Write-Verbose -Message ($NuGetOutput | out-string)
-
-    Pop-Location
+# addins
+if (Test-Path $Addins) {
+    $AddinsDir = Resolve-Path $Addins
+    $ENV:CAKE_PATHS_ADDINS = $AddinsDir
+}
+else {
+    Write-Verbose -Message "Could not find addins directory at $Addins"
 }
 
-# Restore modules from NuGet
-if (Test-Path $MODULES_PACKAGES_CONFIG) {
-    Push-Location
-    Set-Location $MODULES_DIR
-
-    Write-Verbose -Message "Restoring modules from NuGet..."
-    $NuGetOutput = Invoke-Expression "&`"$NUGET_EXE`" install -ExcludeVersion -OutputDirectory `"$MODULES_DIR`""
-
-    if ($LASTEXITCODE -ne 0) {
-        Throw "An error occured while restoring NuGet modules."
-    }
-
-    Write-Verbose -Message ($NuGetOutput | out-string)
-
-    Pop-Location
+# modules
+if (Test-Path $Modules) {
+    $ModulesDir = Resolve-Path $Modules
+    $ENV:CAKE_PATHS_MODULES = $ModulesDir
+}
+else {
+    Write-Verbose -Message "Could not find modules directory at $Modules"
 }
 
 # Make sure that Cake has been installed.
+$CakeDir = Resolve-Path $Cake
+$CAKE_EXE = Join-Path $CakeDir "Cake.exe"
 if (!(Test-Path $CAKE_EXE)) {
     Throw "Could not find Cake.exe at $CAKE_EXE"
 }
+Write-Verbose -Message "Found Cake.exe in PATH at $CAKE_EXE"
 
 # Start Cake
 Write-Host "Running build script..."
